b6f5f513bc9ac0c671e866183e73d66cec29bf7d
Tom Coakes <tomcoakes@gmail.com>
2015-05-01 11:37:39 +0100

    Added feature spec

--- spec/feature/grand_finale_spec.rb
+++ spec/feature/grand_finale_spec.rb
@@ -10,6 +10,15 @@ require 'capybara/rspec'
 
 feature 'Grand Finale' do
 
-  xscenario 'all planes can land and all planes can take off'
+  let(:airport) { Airport.new }
+  let(:six_planes) { Array.new(6, Plane.new) }
+
+  scenario 'all planes can land and all planes can take off' do
+    airport = Airport.new
+    six_planes.each { |plane| airport.land(plane) }
+    six_planes.each { |plane| expect(plane).not_to be_flying }
+    six_planes.each { |plane| airport.take_off(plane) }
+    six_planes.each { |plane| expect(plane).to be_flying }
+  end
 
 end
